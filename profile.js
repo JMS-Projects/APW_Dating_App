feb7bbeb55fcf534aec1dbb3acc7ae515a7d3064
Andrew <griscoma1@students.rowan.edu>
2020-12-11 00:48:28 -0500

    Update profile.js

--- profile.js
+++ profile.js
@@ -25,62 +25,6 @@ class profile
 
     }
 
-    // sets the name
-    setName(name){
-        this.name=name;
-
-    }
-    //sets the age
-    setAge(age){
-        this.age=age;
-
-    }
-    //sets the gender
-    setGender(gender){
-        this.gender=gender;
-
-    }
-    //sets the height
-    setHeight(height){
-        this.height=height;
-
-    }
-    //sets the race
-    setRace(race){
-        this.race=race;
-
-    }
-    //sets the income
-    setIncome(){
-        this.income=income;
-
-    }
-    //sets the religion
-    setReligion(religion){
-        this.religion=religion;
-
-    }
- 
-   
-    // should display the profile information
-    displayProfile()
-    {
-        var page = '<html><head><title>Profile</title></head>' +
-        '<body><h1>Your Profile</h1>' +
-        'Name: ' + this.name+ '<br><br>'+
-        'Age: ' + this.age + '<br><br>' +
-        'Gender: ' + this.gender + '<br><br>' +
-        'Height: ' + this.height + ' cm' + '<br><br>' + 
-        'Race: '+ this.race + '<br><br>' +
-        'Income: ' + this.income + '<br><br>' +
-        'Religion: ' + this.religion + '<br><br>';
-
-        return page;
-
-    }
-
-
-
 
 }
 module.exports = profile;
